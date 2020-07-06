1104aa2d4a50d7296c9482b5804cb3d05bd188a6
brendan <2bndy5@gmail.com>
2020-07-06 12:56:37 -0700

    use quotes & repo name from event obj

--- enter_rmskin_builder.sh
+++ enter_rmskin_builder.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 pip install -r /reqs.txt
-python /rmskin_builder.py --path $1 --version $2 --author $3 --title $4 --dir_out $5
+python /rmskin_builder.py --path "$1" --version "$2" --author "$3" --title "$4" --dir_out "$5"
